79c58edcdaaa252275a59b7730140a8dea2f90f4
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 16:57:00 +0700

    Add TenantContext🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongodbmultitenancy/context/TenantContext.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.mongodbmultitenancy.context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : mongodb-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/01/25
+ * Time: 16.56
+ * To change this template use File | Settings | File Templates.
+ */
+public class TenantContext {
+}

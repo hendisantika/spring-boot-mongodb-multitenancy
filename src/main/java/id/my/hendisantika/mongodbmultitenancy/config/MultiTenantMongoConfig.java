0820b6e12f53d51af5de2df7138b0f0a175b82f2
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 17:00:22 +0700

    Add MultiTenantMongoConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongodbmultitenancy/config/MultiTenantMongoConfig.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.mongodbmultitenancy.config;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : mongodb-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/01/25
+ * Time: 17.00
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public class MultiTenantMongoConfig {
+}

1b485f50c9b59cbfd13b620686ec72eb25f56ea6
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 17:11:15 +0700

    Add Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/mongodbmultitenancy/SpringBootMongodbMultitenancyApplicationTests.java
+++ src/test/java/id/my/hendisantika/mongodbmultitenancy/SpringBootMongodbMultitenancyApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.mongodbmultitenancy;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.mongodbmultitenancy.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mongodb:8.0.4:///employeeDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootMongodbMultitenancyApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        employeeRepository.deleteAll();
     }
 
 }

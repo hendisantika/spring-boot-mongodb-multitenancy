0db00f0cd7e07e66da0ec6a1c5fe1171083b2d2a
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 16:59:38 +0700

    Add multiTenantMongoConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmultitenancy/config/MultiTenantMongoAutoConfiguration.java
+++ src/main/java/id/my/hendisantika/mongodbmultitenancy/config/MultiTenantMongoAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -25,4 +26,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = {"com.sample"})
 @EntityScan(basePackages = {"com.sample"})
 public class MultiTenantMongoAutoConfiguration {
+    @Bean
+    public MultiTenantMongoConfig multiTenantMongoConfig(final MultiTenantMongoProperties multiTenantMongoProperties) {
+        return new MultiTenantMongoConfig(multiTenantMongoProperties);
+    }
 }

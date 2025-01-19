c1b3a4d71231f10d44edfecf4fdb5621da4b325a
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 16:55:54 +0700

    Add EmployeeService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongodbmultitenancy/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.mongodbmultitenancy.service;
+
+import id.my.hendisantika.mongodbmultitenancy.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : mongodb-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/01/25
+ * Time: 16.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+}

255e6ce6efe545e09d92578461c7ffbf18bc3620
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 17:03:41 +0700

    Add getEmployeeById in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmultitenancy/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/mongodbmultitenancy/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.mongodbmultitenancy.controller;
 
+import id.my.hendisantika.mongodbmultitenancy.domain.Employee;
 import id.my.hendisantika.mongodbmultitenancy.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private EmployeeService employeeService;
+
+    @GetMapping(path = "/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") final String id) {
+        return new ResponseEntity(employeeService.findById(id), HttpStatus.OK);
+    }
 }
